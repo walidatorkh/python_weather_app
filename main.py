1bc12174808b56f9628a6e1a31bdf920d554ac42
walidatorkh <walidatorkh@gmail.com>
2024-04-25 10:27:09 +0300

    second commit

--- main.py
+++ main.py
@@ -4,14 +4,31 @@ import os
 
 
 def get_weather(city_name):
+    """
+    Fetches the weather information for a given city.
+
+    Parameters:
+    city_name (str): The name of the city for which to fetch the weather information.
+
+    Returns:
+    dict: A dictionary containing the weather information for the city.
+    """
     load_dotenv()
-    # city_name = input("Enter required city name: ")
     API_KEY = os.getenv("API_KEY")
     query_string = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric"
-    responce = requests.get(query_string).json()
-    return responce
+    response = requests.get(query_string).json()
+    return response
 
 
-def print_weather_info(responce):
+def print_weather_info(response):
+    """
+    Prints the weather information for a city.
+
+    Parameters:
+    response (dict): A dictionary containing the weather information for a city.
+
+    Returns:
+    None
+    """
     print(f"Weather information: ")
-    print(f"{responce['name']} - {responce['main']['temp']}°C")
+    print(f"{response['name']} - {response['main']['temp']}°C")
